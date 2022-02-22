99e9870061c2200d35b25ecdcf7ab53a11885a71
Jonathan Dolbee <jdolbe1@lsu.edu>
2022-02-22 10:31:34 -0600

    - Fixed requirments.txt for recent versions

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ class CleanCommand(Command):
 
 setup(
     name="parsedan",
-    version="0.0.3a02",
+    version="0.0.3a03",
     description="A shodan parser that given a query will download results and parse them into CSV or JSON files while also scoring them.",
     long_description="README",
     long_description_content_type="text/markdown",
